f816faff7ea351384ea85c55f542be403a851b1c
newnakashima <newnakashima2014@gmail.com>
2019-12-31 22:01:46 +0900

    add karabiner setting

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ln -Ffs $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
-ln -Ffs $HOME/dotfiles/.zshrc     $HOME/.zshrc
-ln -Ffs $HOME/dotfiles/.vimrc     $HOME/.vimrc
-ln -Ffs $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln -Ffs $HOME/dotfiles/.tmux.conf       $HOME/.tmux.conf
+ln -Ffs $HOME/dotfiles/.zshrc           $HOME/.zshrc
+ln -Ffs $HOME/dotfiles/.vimrc           $HOME/.vimrc
+ln -Ffs $HOME/dotfiles/.gitconfig       $HOME/.gitconfig
+ln -Ffs $HOME/dotfiles/karabiner.json   $HOME/.config/karabiner/karabiner.json
