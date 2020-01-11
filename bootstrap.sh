1c9372558bede88f57af9ab49cddcdc5b898a1dd
newnakashima <newnakashima2014@gmail.com>
2020-01-11 19:01:06 +0900

    detach key

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,7 @@ ln -Ffs $HOME/dotfiles/.zshrc           $HOME/.zshrc
 ln -Ffs $HOME/dotfiles/.vimrc           $HOME/.vimrc
 ln -Ffs $HOME/dotfiles/.gitconfig       $HOME/.gitconfig
 ln -Ffs $HOME/dotfiles/karabiner.json   $HOME/.config/karabiner/karabiner.json
+
+# Docker
+ln -Ffs $HOME/dotfiles/.docker/config.json $HOME/.docker/config.json
+ln -Ffs $HOME/dotfiles/.docker/daemon.json $HOME/.docker/daemon.json
