25bd671eb3407b75c86f1d1e2e2a122f86e11879
newnakashima <newnakashima2014@gmail.com>
2020-02-16 13:25:01 +0900

    messages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
 
-# install Tmux Plugin Manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+set -euo pipefail
 
+echo 'install dependencies'
+
+# Tmux Plugin Manager
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+    echo 'tpm has been installed already.'
+fi
+
+# vimplug
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+else
+    echo 'vimplug has been installed already.'
+fi
+
+echo 'create symlinks.'
+
+echo "create symlink to $HOME/.tmux.conf"
 ln -Ffs $HOME/dotfiles/.tmux.conf       $HOME/.tmux.conf
+
+echo "create symlink to $HOME/.zshrc"
 ln -Ffs $HOME/dotfiles/.zshrc           $HOME/.zshrc
+
+echo "create symlink to $HOME/.vimrc"
 ln -Ffs $HOME/dotfiles/.vimrc           $HOME/.vimrc
+
+echo "create symlink to $HOME/.gitconfig"
 ln -Ffs $HOME/dotfiles/.gitconfig       $HOME/.gitconfig
+
+echo "create symlink to $HOME/.config/karabiner/karabiner.json"
 ln -Ffs $HOME/dotfiles/karabiner.json   $HOME/.config/karabiner/karabiner.json
 
 # Docker
+echo "create symlink to $HOME/.docker/config.json"
 ln -Ffs $HOME/dotfiles/.docker/config.json $HOME/.docker/config.json
+
+echo "create symlink to $HOME/.docker/daemon.json"
 ln -Ffs $HOME/dotfiles/.docker/daemon.json $HOME/.docker/daemon.json
 
-# vimplug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo 'bootstrap.sh is successfully done.'
